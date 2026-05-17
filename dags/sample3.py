39246a8579972d18e6146a1d9f8d87653b9e5db9
Arques <bhuvaneshkpb@gmail.com>
2026-05-17 12:09:33 +0530

    sample3 for sqlOperator for postgres ETL completed

--- dags/sample3.py
+++ dags/sample3.py
@@ -1,33 +1,93 @@
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.sdk import task
+from airflow.sdk import task,dag
 import os
 import requests
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+import pendulum
+import datetime
 
-create_employee = SQLExecuteQueryOperator(
-    task_id= "create employee table",
-    conn_id="connecting_airlfow_with_postgres_sql",
-    sql="""
-CREATE TABLE IF NOT EXISTS employees (
-            "Serial Number" NUMERIC PRIMARY KEY,
-            "Company Name" TEXT,
-            "Employee Markme" TEXT,
-            "Description" TEXT,
-            "Leave" INTEGER
-        );""",
+@dag(
+    dag_id="process_employees",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
 )
+def ProcessEmployees():
+    create_employees_table = SQLExecuteQueryOperator(
+        task_id="create_employees_table",
+        conn_id="connecting_airlfow_with_postgres_sql",
+        sql="""
+            CREATE TABLE IF NOT EXISTS employees (
+                "Serial Number" NUMERIC PRIMARY KEY,
+                "Company Name" TEXT,
+                "Employee Markme" TEXT,
+                "Description" TEXT,
+                "Leave" INTEGER
+            );""",
+    )
 
-create_employee_temporay_table = SQLExecuteQueryOperator(
-    task_id= "create employee temporary table",
-    conn_id="connecting_airlfow_with_postgres_sql",
-    sql="""
-DROP TABLE IF EXISTS employees_temp;
-        CREATE TABLE employees_temp (
-            "Serial Number" NUMERIC PRIMARY KEY,
-            "Company Name" TEXT,
-            "Employee Markme" TEXT,
-            "Description" TEXT,
-            "Leave" INTEGER,
-            "Joke Variable" INTEGER,
-        );""",
-)
+    create_employees_temp_table = SQLExecuteQueryOperator(
+        task_id="create_employees_temp_table",
+        conn_id="connecting_airlfow_with_postgres_sql",
+        sql="""
+            DROP TABLE IF EXISTS employees_temp;
+            CREATE TABLE employees_temp (
+                "Serial Number" NUMERIC PRIMARY KEY,
+                "Company Name" TEXT,
+                "Employee Markme" TEXT,
+                "Description" TEXT,
+                "Leave" INTEGER
+            );""",
+    )
+
+    @task
+    def get_data(): 
+        data_path = "/opt/airflow/dags/files/employees.csv"
+        os.makedirs(os.path.dirname(data_path), exist_ok=True)
+
+        url = "https://raw.githubusercontent.com/apache/airflow/main/airflow-core/docs/tutorial/pipeline_example.csv"
+
+        response = requests.request("GET", url)
+
+        with open(data_path, "w") as file:
+            file.write(response.text)
+
+        postgres_hook = PostgresHook(postgres_conn_id="connecting_airlfow_with_postgres_sql")
+        conn = postgres_hook.get_conn()
+        cur = conn.cursor()
+        with open(data_path, "r") as file:
+            cur.copy_expert(
+                "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                file,
+            )
+        conn.commit()
+
+    @task
+    def merge_data():
+        query = """
+            INSERT INTO employees
+            SELECT *
+            FROM (
+                SELECT DISTINCT *
+                FROM employees_temp
+            ) t
+            ON CONFLICT ("Serial Number") DO UPDATE
+            SET
+              "Employee Markme" = excluded."Employee Markme",
+              "Description" = excluded."Description",
+              "Leave" = excluded."Leave";
+        """
+        try:
+            postgres_hook = PostgresHook(postgres_conn_id="connecting_airlfow_with_postgres_sql")
+            conn = postgres_hook.get_conn()
+            cur = conn.cursor()
+            cur.execute(query)
+            conn.commit()
+            return 0
+        except Exception as e:
+            return 1
+    
+    [create_employees_table, create_employees_temp_table] >> get_data() >> merge_data()
+
+dag = ProcessEmployees()
